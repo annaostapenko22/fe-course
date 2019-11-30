532ff4a0c495eb5d119f452089269bff9deb40a1
Anna Ostapenko <kvintabudinvest@gmail.com>
2019-12-01 00:01:59 +0200

    changed structure

--- JavaScript/module-13/src/index.js
+++ JavaScript/module-13/src/index.js
@@ -1,41 +1,39 @@
 import './styles.css';
 import axios from 'axios';
 import imgTemplate from './templates/img.hbs';
+import { setTimeout } from 'timers';
 
-const imgContainer = document.querySelector('.gallery');
-const searchField = document.querySelector('.search-form');
-const loadBtn = document.querySelector('.button--load-more');
-const upBtn = document.querySelector('.up');
-const photoCard = document.querySelector('.photo-card');
-const photoCardItem = document.querySelector('.photo-card img');
-const lightBox = document.querySelector('.js-lightbox');
-const lightBoxBtn = document.querySelector('.lightbox__button');
-const likes = document.querySelector('.likes');
-const comments = document.querySelector('.comments');
-const downloads = document.querySelector('.downloads');
-const shows = document.querySelector('.shows');
-
-console.log(lightBox);
-console.log(photoCard);
-
-console.log(loadBtn);
+const refs = {
+  imgContainer: document.querySelector('.gallery'),
+  searchField: document.querySelector('.search-form'),
+  loadBtn: document.querySelector('.button--load-more'),
+  upBtn: document.querySelector('.up'),
+  photoCard: document.querySelector('.photo-card'),
+  photoCardItem: document.querySelector('.photo-card img'),
+  lightBox: document.querySelector('.lightbox'),
+  lightBoxBtn: document.querySelector('.lightbox__button'),
+  likes: document.querySelector('.likes'),
+  comments: document.querySelector('.comments'),
+  downloads: document.querySelector('.downloads'),
+  shows: document.querySelector('.shows'),
+  loader: document.querySelector('.loader-wrapper'),
+  stats: document.querySelector('.stats'),
+};
 
 const getImages = (name, page) => {
   return axios.get(
-    `https://pixabay.com/api/?key=14452993-a97f0aeb82bfc4ebb60896535&q=${name}&per_page=20&page=${page}`,
+    `https://pixabay.com/api/?key=14452993-a97f0aeb82bfc4ebb60896535&q=${name}&per_page=70&page=${page}`,
   );
 };
 let page = 1;
 let searchQuery = '';
-loadBtn.classList.add('unvisible');
+refs.loadBtn.classList.add('unvisible');
 function searchFormHandler(evt) {
   evt.preventDefault();
-  loadBtn.classList.add('unvisible');
+  refs.loadBtn.classList.add('unvisible');
   searchQuery = evt.currentTarget.elements.query.value;
-  console.log(searchQuery);
-  // let page = 1;
-  imgContainer.innerHTML = '';
-  searchField.elements.query.value = '';
+  refs.imgContainer.innerHTML = '';
+  refs.searchField.elements.query.value = '';
 
   page = 1;
   drawImages();
@@ -43,13 +41,9 @@ function searchFormHandler(evt) {
 
 function drawImages() {
   getImages(searchQuery, page).then(({ data }) => {
-    console.log(data.hits);
-    console.log(page);
-
     data.hits.forEach(elem => {
-      console.log(elem);
-      imgContainer.insertAdjacentHTML('beforeend', imgTemplate(elem));
-      loadBtn.classList.remove('unvisible');
+      refs.imgContainer.insertAdjacentHTML('beforeend', imgTemplate(elem));
+      refs.loadBtn.classList.remove('unvisible');
     });
     incrementPage();
   });
@@ -68,24 +62,29 @@ function scroll() {
 }
 function showPreview(evt) {
   if (evt.target.nodeName === 'IMG') {
-    lightBox.classList.add('is-open');
+    refs.loader.style.display = 'flex';
+    refs.lightBox.classList.add('is-open');
+    refs.photoCard.classList.add('isBlur');
     const getImg = event.target.dataset.source;
     const getComments = event.target.dataset.comments;
     const getLikes = event.target.dataset.likes;
     const getViews = event.target.dataset.shows;
     const getDownloads = event.target.dataset.downloads;
-    console.log(getLikes);
-    photoCardItem.setAttribute('src', getImg);
-    likes.textContent = getLikes;
-    comments.textContent = getComments;
-    shows.textContent = getViews;
-    downloads.textContent = getDownloads;
-    console.log(evt.target.nodeName);
+    refs.photoCardItem.setAttribute('src', getImg);
+
+    setTimeout(function() {
+      refs.loader.style.display = 'none';
+      refs.photoCard.classList.remove('isBlur');
+    }, 800);
+    refs.likes.textContent = getLikes;
+    refs.comments.textContent = getComments;
+    refs.shows.textContent = getViews;
+    refs.downloads.textContent = getDownloads;
   }
 }
 
 function removeShowPreview() {
-  lightBox.classList.remove('is-open');
+  refs.lightBox.classList.remove('is-open');
 }
 
 function handleBackDropClick(evt) {
@@ -94,10 +93,10 @@ function handleBackDropClick(evt) {
   }
   removeShowPreview();
 }
-console.log(lightBoxBtn);
-upBtn.addEventListener('click', scroll);
-searchField.addEventListener('submit', searchFormHandler);
-loadBtn.addEventListener('click', drawImages);
-imgContainer.addEventListener('click', showPreview);
-lightBoxBtn.addEventListener('click', removeShowPreview);
+
+refs.upBtn.addEventListener('click', scroll);
+refs.searchField.addEventListener('submit', searchFormHandler);
+refs.loadBtn.addEventListener('click', drawImages);
+refs.imgContainer.addEventListener('click', showPreview);
+refs.lightBoxBtn.addEventListener('click', removeShowPreview);
 window.addEventListener('click', handleBackDropClick);
